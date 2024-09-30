abf027fe119fa71c6194fe425823947a461272eb
Dhileepguru <dhileepguru497@gmail.com>
2024-09-30 15:38:25 +0530

    Created a Displaying task list

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,25 @@
 const inputValue=document.getElementById("inputValue");
-displayValue
+const displayTask=document.getElementById("taskList");
+
+function displayValue()
+{
+    const task=inputValue.value.trim();
+    //alert();
+    if(task)
+    {
+        createTaskElement(task);
+        inputValue.value='';
+    }
+}
+
+function createTaskElement(task)
+{
+    const listItem=document.createElement('li');
+    listItem.textContent=task;
+    displayTask.appendChild(listItem);
+}
+
+function deleteValue()
+{
+    
+}
